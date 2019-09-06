3eee4bdec27c8210d6811529d72e4d86c6d84420
EC2 Default User <ec2-user@ip-172-31-80-112.ec2.internal>
2019-09-06 07:25:04 +0000

    pushing files for Lab 1

---
+++ assets/scripts.js
@@ -0,0 +1,80 @@
+
+// Slide Animation
+  let slideUp = (target, duration=500) => {
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.boxSizing = 'border-box';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout( () => {
+      target.style.display = 'none';
+      target.style.removeProperty('box-sizing');
+      target.style.removeProperty('padding-top');
+      target.style.removeProperty('padding-bottom');
+      target.style.removeProperty('margin-top');
+      target.style.removeProperty('margin-bottom');
+      target.style.removeProperty('height');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+      //alert("!");
+    }, duration);
+  }
+
+  let slideDown = (target, duration=500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+
+    if (display === 'none')
+      display = 'block';
+
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.boxSizing = 'border-box';
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    window.setTimeout( () => {
+      target.style.removeProperty('box-sizing');
+      target.style.removeProperty('padding-top');
+      target.style.removeProperty('padding-bottom');
+      target.style.removeProperty('margin-top');
+      target.style.removeProperty('margin-bottom');
+      target.style.removeProperty('height');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+    }, duration);
+  }
+  
+  var link = document.querySelector('.js-link'),
+    linkIcon = document.querySelector('.js-link-icon'),
+    linkList = document.querySelector('.js-link-list');
+
+
+// Event listener
+link.addEventListener('click', function() {
+  this.classList.toggle("active");
+  linkList.classList.toggle("active");
+  if(this.classList.contains("active")){
+    linkIcon.textContent = "-";
+    slideDown(linkList, 500);
+  }
+  else{
+    linkIcon.textContent = "+";
+    slideUp(linkList, 500);
+  }
+});
